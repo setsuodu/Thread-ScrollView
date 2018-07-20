1d2ed66f3820a126077544283e4676ed06eeb8c3
薛宇涛 <xueyutao@xueyutaodeMacBook-Air.local>
2018-07-20 17:35:31 +0800

    ScrollView Topic

---
+++ Assets/Scripts/DragScrollTopic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragScrollTopic : MonoBehaviour
+{
+	public ScrollRect m_scrollRect;
+	public RectTransform m_image;
+	public float _y;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+        _y = m_scrollRect.content.anchoredPosition.y / 500;
+
+		if(_y < 0)
+		{
+			m_image.localScale = Vector3.one * (1 - _y);
+		}
+		else if(_y > 0 && _y < 0.6f)
+		{
+			m_image.anchoredPosition = new Vector2(0, _y * 500);
+            m_image.localScale = Vector3.one;
+		}
+		else
+		{
+			m_image.anchoredPosition = new Vector2(0, 300);
+            m_image.localScale = Vector3.one;
+		}
+	}
+
+	public void OnDrag()
+	{
+		//Debug.Log("OnDrag -> " + m_scrollRect.content.anchoredPosition);
+	}
+
+	public void OnDrop()
+	{
+		//Debug.Log("OnDrop -> " + m_scrollRect.content.anchoredPosition);
+	}
+}
